7bee5fbdc7454d9e373873bfe0f9e2c4558a070d
Moshimoshi <1973049893@qq.com>
2020-08-28 17:49:02 +1000

    Provide APIs from back end

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-COMP30022
-Bounty Programmers Repository.
-Yutao
+APIs:
+Register  --
+   1. relative url:  /home/register      absolute url: http://localhost:3000/home/register
+   2. method:  POST
+   3. HTTP request Header: Content-Type application/json
+   4. HTTP request Body example, all the following field should be filled
+         NOTE: password should be 123456 now to avoid further headache when testing login,
+         password2 should be the same as password1, which aims at confirming the password
+           {
+             "first_name": "TomCat",
+             "family_name": "One",
+             "email": "12312@gmail.com",
+             "password": "123456",
+             "password2": "123456"
+           }
+   5. send the HTTP request
+
+Login --
+      1. relative url:  /home/login       absolute url: http://localhost:3000/home/login
+      2. method:  POST
+      3. HTTP request Header: Content-Type application/json 
+      4. HTTP request Body example, all the following field should be filled
+                {
+                   "email":  "12312@gmail.com",
+                   "12312@gmail.com"
+                 }
+      5. send the HTTP request
--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+// APIs:
+// Register  --
+//    1. relative url:  /home/register      absolute url: http://localhost:3000/home/register
+//    2. method:  POST
+//    3. HTTP request Header: Content-Type application/json
+//    4. HTTP request Body example, all the following field should be filled
+//          NOTE: password should be 123456 now to avoid further headache when testing login,
+//          password2 should be the same as password1, which aims at confirming the password
+//            {
+//              "first_name": "TomCat",
+//              "family_name": "One",
+//              "email": "12312@gmail.com",
+//              "password": "123456",
+//              "password2": "123456"
+//            }
+//     5. send the HTTP request
+//
+// Login --
+//       1. relative url:  /home/login       absolute url: http://localhost:3000/home/login
+//       2. method:  POST
+//       3. HTTP request Header: Content-Type application/json
+//       4. HTTP request Body example, all the following field should be filled
+//                 {
+//                    "email":  "12312@gmail.com",
+//                    "12312@gmail.com"
+//                  }
+//       5. send the HTTP request
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -68,7 +95,7 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use('/', index/Router);
+app.use('/', indexRouter);
 app.use('/home', homeRouter);
 
 // catch 404 and forward to error handler
