3a7081657da10dc979146d03c7dd714b9ae97b65
PeiyuOwO <56665146+PeiyuOwO@users.noreply.github.com>
2020-08-29 17:47:01 +1000

    Add files via upload

---
+++ temp1Homepage/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
