26aa2ad375449c0fb6a74c10f4a94d7892a4e44a
Haoyuan Yu <haoyuan.yu@student.unimelb.edu.au>
2020-08-30 16:49:50 +1000

    update from master branch

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import "./css/app.scss";
-import "./css/style.scss";
-import Router from './router.js';
-
-ReactDOM.render(<Router/>, document.getElementById('root'));
