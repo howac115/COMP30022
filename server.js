2553f7b8806df672c7278aec031fdb01b947b986
haoqic <39285061+howac115@users.noreply.github.com>
2020-09-25 19:26:39 +1000

    remove mongoose deprecation warning

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 const db = require('./config/keys').mongoURL;
 
 // Connect to MongoDB
-mongoose.connect(db)
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
