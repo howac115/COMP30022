ec5490747750c8260795af29628200a28c90a4f8
Hao Qi Chen <haoqic@student.unimelb.edu.au>
2020-09-29 00:29:16 +1000

    add template bugs

--- client/src/pages/template.js
+++ client/src/pages/template.js
@@ -8,14 +8,18 @@ import { Card, Col, Typography, Row } from 'antd';
 import 'antd/dist/antd.css';
 import { ShareAltOutlined, EyeOutlined, PlusOutlined } from '@ant-design/icons';
 import { useHistory } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
 
 const { Title } = Typography;
 const { Meta } = Card;
 
 export default function Template(props) {
+
     let history = useHistory();
+    const { handleSubmit } = useForm();
     const [folios, setFolios] = useState([]);
+    const [name, setName] = useState('');
 
     useEffect(() => {
         document.title = 'ExPortfolio | Template';
@@ -31,52 +35,57 @@ export default function Template(props) {
 
     const handleCreate = async prop => {
         const user = global.auth.getUser().id;
-        await axios.post('/folio/' + user + '/clone', {
+        console.log(prop)
+        await axios.post('/folio/clone', {
             user: user,
-            name: prop,
+            name: name,
+            content: prop.content
         });
         toast.success(prop + ' succeccful cloned');
         history.push('/folios');
     };
+
+    const handleChange = e => {
+        setName({ name: e.target.value });
+    }
 
     const askCreate = prop => {
         confirmAlert({
             customUI: ({ onClose }) => {
                 return (
                     <div className="container">
-                        <div>
-                            <h1>Add template to folios</h1>
-                            <br></br>
-                        </div>
-                        <input
-                            type="text"
-                            placeholder="My Awesome Portfolio"
-                            name="name"
-                        />
-                        <br></br><br></br>
-                        <div className="field is-grouped">
-                            <div className="control">
-                                <button
-                                    className="button is-info"
-                                    onClick={() => {
-                                        onClose(prop);
-                                        handleCreate(prop);
-                                    }}
-                                >
-                                    OK
-                                </button>
-                            </div>
-                            <div className="control">
-                                <button
-                                    className="button is-light"
-                                    onClick={onClose}
-                                >
-                                    Cancel
-                                </button>
-                            </div>
-                        </div>
+                    <form onSubmit={handleSubmit(handleCreate.bind(this, prop))}>
+            <div>
+                <h1>Add template to folios</h1>
+                <br></br>
+                </div>
+                <input
+                type="text"
+                placeholder="My Awesome Portfolio"
+                name="clonedName"
+                onChange={handleChange}
+                />
+                <br></br><br></br>
+                <div className="field is-grouped">
+                    <div className="control">
+                    <button
+                className="button is-info"
+                    >
+                    OK
+                    </button>
                     </div>
-                );
+                    <div className="control">
+                    <button
+                className="button is-light"
+                onClick={onClose}
+                    >
+                    Cancel
+                    </button>
+                    </div>
+                    </div>
+                    </form>
+                    </div>
+            );
             },
         });
     };
@@ -85,35 +94,35 @@ export default function Template(props) {
         if (global.auth.getUser().id !== folio.user) {
             return [
                 <button className="button is-light">
-                    <a href={'/' + folio.user + '/' + folio.name}>
-                        {' '}
-                        <EyeOutlined />
-                    </a>
+                <a href={'/' + folio.user + '/' + folio.name}>
+                {' '}
+                <EyeOutlined />
+                </a>
                 </button>,
                 <button
-                    className="button is-light"
-                    onClick={handleShare.bind(this, folio)}
+            className="button is-light"
+            onClick={handleShare.bind(this, folio)}
                 >
-                    <ShareAltOutlined />
+                <ShareAltOutlined />
                 </button>, <button
-                    className="button is-light"
-                    onClick={askCreate.bind(this, folio)}
+            className="button is-light"
+            onClick={askCreate.bind(this, folio)}
                 >
-                    <PlusOutlined />
+                <PlusOutlined />
                 </button>]
         } else {
             return [<button className="button is-light">
                 <a href={'/' + folio.user + '/' + folio.name}>
-                    {' '}
-                    <EyeOutlined />
+                {' '}
+                <EyeOutlined />
                 </a>
-            </button>,
-            <button
-                className="button is-light"
-                onClick={handleShare.bind(this, folio)}
-            >
+                </button>,
+                <button
+            className="button is-light"
+            onClick={handleShare.bind(this, folio)}
+                >
                 <ShareAltOutlined />
-            </button>]
+                </button>]
         }
     }
 
@@ -127,42 +136,42 @@ export default function Template(props) {
     const renderCards = folios.map((folio, index) => {
         return (
             <Col key={index} lg={8} md={12} xs={24}>
-                <Draggable>
-                    <Card
-                        hoverable
-                        style={{ width: 300, marginTop: 16 }}
-                        actions={renderOptions(folio)}
-                    >
-                        <Meta
-                            title={folio.name}
-                            description="This is the description"
-                        />
-                        <div
-                            style={{
-                                height: 150,
-                                overflowY: 'scroll',
-                                marginTop: 10,
-                            }}
-                        >
-                            <div
-                                dangerouslySetInnerHTML={{
-                                    __html: folio.content,
-                                }}
-                            />
-                        </div>
-                    </Card>
-                </Draggable>
-            </Col>
-        );
+            <Draggable>
+            <Card
+        hoverable
+        style={{ width: 300, marginTop: 16 }}
+        actions={renderOptions(folio)}
+            >
+            <Meta
+        title={folio.name}
+        description="This is the description"
+            />
+            <div
+        style={{
+            height: 150,
+                overflowY: 'scroll',
+                marginTop: 10,
+        }}
+    >
+    <div
+        dangerouslySetInnerHTML={{
+            __html: folio.content,
+        }}
+        />
+        </div>
+        </Card>
+        </Draggable>
+        </Col>
+    );
     });
 
     return (
         <div>
-            <Layout />
-            <div style={{ width: '85%', margin: '3rem auto' }}>
-                <Title level={2}> Templates </Title>
-                <Row gutter={[32, 16]}>{renderCards}</Row>
-            </div>
-        </div>
-    );
+        <Layout />
+        <div style={{ width: '85%', margin: '3rem auto' }}>
+<Title level={2}> Templates </Title>
+        <Row gutter={[32, 16]}>{renderCards}</Row>
+    </div>
+    </div>
+);
 }
